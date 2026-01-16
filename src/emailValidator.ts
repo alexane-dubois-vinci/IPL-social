1761e3f74b1b6d4c17de0bb29b9b551446f3b70b
alexane-dubois-vinci <alexane.dubois@student.vinci.be>
2026-01-16 16:38:16 +0100

    add: validateEmail test - returns false if no characters prior to @

--- src/emailValidator.ts
+++ src/emailValidator.ts
@@ -1,6 +1,9 @@
 export function validateEmail(email: string): boolean {
     if (!email.includes("@")) return false;
 
+    const emailSections = email.split("@");
+    if (!emailSections[0] || emailSections[0].length === 0) return false;
+
     return true;
 }
 
--- test/emailValidator.test.ts
+++ test/emailValidator.test.ts
@@ -4,4 +4,8 @@ describe("Email Validator", () => {
     it("should return false when given an email that does not contain @", () => {
         expect(validateEmail("emailexample")).toBe(false);
     });
+
+    it("should return false when given an email that with no characters prior to @", () => {
+        expect(validateEmail("@vinci.be")).toBe(false);
+    })
 })
